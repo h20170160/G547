8d3f2d8042e605a62f6c0eab85f28de92d1228e8
h20170160 <35378743+h20170160@users.noreply.github.com>
2018-01-17 19:29:17 +0530

    Update mymodule.c

--- G547/assignment1/mymodule.c
+++ G547/assignment1/mymodule.c
@@ -3,13 +3,13 @@
 #include <linux/init.h> // for printk()
 
 
-static int __init mod_init(void){
+static int mod_init(void){
 	printk(KERN_INFO "Hello!\n");
 	return 0;
 }
 
 
-static void __exit mod_exit(void){
+static void mod_exit(void){
 	printk(KERN_INFO "Bye!\n");
 }
 
