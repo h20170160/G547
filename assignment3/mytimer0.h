abb552bd9d7daefe3f5406b4b9d3f74054e3f74b
h20170160 <35378743+h20170160@users.noreply.github.com>
2018-02-13 10:22:41 +0530

    Create mytimer0.h

---
+++ assignment3/mytimer0.h
@@ -0,0 +1,17 @@
+#ifndef MYTIMER0_H
+#define MYTIMER0_H
+
+
+
+#define DEVICE_FILE_NAME "/dev/timer0"
+
+#define SUCCESS 0
+#define DEVICE_NAME "timer0"
+#define IOCTL_DELAY _IOR('a', 0, char *)
+
+
+
+#endif
+
+
+
